8e818200e585023b95906903b5c3aa83e1103bb7
sxf <146811+owls@user.noreply.gitee.com>
2025-06-08 21:14:12 +0800

    支持ios13以上uiwindowscene的场景

--- AMToast/Classes/AMToastWindow.swift
+++ AMToast/Classes/AMToastWindow.swift
@@ -1,17 +1,20 @@
 import UIKit
 
 final class AMToastWindow: UIWindow {
-    static let shared = AMToastWindow(frame: UIScreen.main.bounds)
+    static let shared: AMToastWindow = {
+        let window = AMToastWindow(frame: UIScreen.main.bounds)
+        window.setupWindow()
+        return window
+    }()
+    
     private var toastViewController: AMToastViewController?
     
     private override init(frame: CGRect) {
         super.init(frame: frame)
-        setupWindow()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setupWindow()
     }
     
     private func setupWindow() {
@@ -20,8 +23,31 @@ final class AMToastWindow: UIWindow {
         isUserInteractionEnabled = false
         isHidden = true
         
+        // 设置 windowScene
+        if #available(iOS 13.0, *) {
+            updateWindowScene()
+        }
+        
         toastViewController = AMToastViewController()
         rootViewController = toastViewController
+    }
+    
+    @available(iOS 13.0, *)
+    private func updateWindowScene() {
+        // 优先使用活跃的场景
+        if let activeScene = UIApplication.shared.connectedScenes
+            .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            self.windowScene = activeScene
+        }
+        // 如果没有活跃场景，使用第一个前台场景
+        else if let foregroundScene = UIApplication.shared.connectedScenes
+            .first(where: { $0.activationState == .foregroundInactive }) as? UIWindowScene {
+            self.windowScene = foregroundScene
+        }
+        // 如果都没有，使用第一个可用场景
+        else if let firstScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            self.windowScene = firstScene
+        }
     }
     
     func addToastViewToQueue(_ toastView: UIView, position: AMToastViewPosition, duration: TimeInterval, delay: TimeInterval) {
@@ -29,6 +55,9 @@ final class AMToastWindow: UIWindow {
     }
     
     func addToastViewToQueue(_ toastView: UIView, position: AMToastViewPosition, duration: TimeInterval) {
+        if #available(iOS 13.0, *) {
+            updateWindowScene()
+        }
         show()
         toastViewController?.addToastViewToQueue(toastView, position: position, duration: duration)
     }
