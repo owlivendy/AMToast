d83be93daf40cbb73665716e7968dc614e672b59
sxf <146811+owls@user.noreply.gitee.com>
2025-06-09 14:56:51 +0800

    fix: 优化资源文件访问方式，同时兼容 CocoaPods 和 SPM

--- Sources/AMToast/UIImage+AMToast.swift
+++ Sources/AMToast/UIImage+AMToast.swift
@@ -2,11 +2,16 @@ import UIKit
 
 extension UIImage {
     static func am_toastImage(named name: String) -> UIImage? {
+        #if SWIFT_PACKAGE
+        let bundle = Bundle.module
+        return UIImage(named: name, in: bundle, compatibleWith: nil)
+        #else
         let bundle = Bundle(for: AMToast.self)
         if let bundlePath = bundle.path(forResource: "AMToast", ofType: "bundle"),
            let resourceBundle = Bundle(path: bundlePath) {
             return UIImage(named: name, in: resourceBundle, compatibleWith: nil)
         }
         return nil
+        #endif
     }
 } 
